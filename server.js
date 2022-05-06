4c1afc78b89683883da1849d54a4340db54c4b83
dextercrypt <36314791+dextercrypt@users.noreply.github.com>
2022-05-07 01:18:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.use((error, req, res, next) => {
 });
 
 // App's connection port
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is connected on port ${PORT}`);
